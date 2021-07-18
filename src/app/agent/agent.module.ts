e1bc23fc185e0199d4e810a78bdf31dd3b156959
itai <itaiz@gene.co.zw>
2021-07-18 13:31:03 +0200

    Major refactoring

--- src/app/agent/agent.module.ts
+++ src/app/agent/agent.module.ts
@@ -3,10 +3,12 @@ import { CommonModule } from '@angular/common';
 
 import { AgentRoutingModule } from './agent-routing.module';
 import { HomeComponent } from './home/home.component';
+import { RateuserComponent } from './rateuser/rateuser.component';
+import { ProfileComponent } from './profile/profile.component';
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, RateuserComponent, ProfileComponent],
   imports: [
     CommonModule,
     AgentRoutingModule
